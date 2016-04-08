751c524f1ddf5e18e74f590c55e71517eea5cb6b
cqc <1016903103@qq.com>
2016-04-08 23:14:07 +0800

    deal error

--- lib/getproduct.py
+++ lib/getproduct.py
@@ -21,22 +21,37 @@ def get_product(url, fail_time = 0):
     except TimeoutException:
         print u'请求超时, 正在切换代理, 继续重试'
         new_proxy_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前失败次数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        get_product(url, fail_time)
     except socket.error:
         print u'请求宝贝过于频繁, 请求被中断, 正在切换会话重试'
         new_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前失败次数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        get_product(url, fail_time)
     except urllib2.URLError:
         print u'请求宝贝过于频繁, 发生网络错误, 正在切换会话重试'
         new_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前失败次数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        get_product(url, fail_time)
     except WindowsError:
         print u'未知错误, 跳过继续运行'
     except OSError:
         print u'未知错误, 跳过继续运行'
-    finally:
-        fail_time = fail_time + 1
-        if config.CONSOLE_OUTPUT:
-            print u'当前页面请求失败数', fail_time
-        if fail_time == config.MAX_FAIL:
-            if config.CONSOLE_OUTPUT:
-                print '失败次数过多, 跳过此请求'
-            return False
-        get_product(url, fail_time)
--- lib/main.py
+++ lib/main.py
@@ -35,12 +35,36 @@ def scrap(url, fail_time=0):
     except TimeoutException:
         print u'请求超时, 正在切换代理, 继续重试'
         new_proxy_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前页面请求失败数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        scrap(url, fail_time)
     except socket.error:
         print u'请求页面过于频繁, 请求被中断, 正在切换会话重试'
         new_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前页面请求失败数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        scrap(url, fail_time)
     except urllib2.URLError:
         print u'请求页面过于频繁, 发生网络错误, 正在切换会话重试'
         new_driver()
+        fail_time = fail_time + 1
+        if config.CONSOLE_OUTPUT:
+            print u'当前页面请求失败数', fail_time
+        if fail_time == config.MAX_FAIL:
+            if config.CONSOLE_OUTPUT:
+                print u'失败次数过多, 跳过此请求'
+            return False
+        scrap(url, fail_time)
     except WindowsError:
         print u'未知错误, 跳过继续运行'
     except OSError:
@@ -48,15 +72,6 @@ def scrap(url, fail_time=0):
     except Exception:
         print u'未知错误'
 
-    finally:
-        fail_time = fail_time + 1
-        if config.CONSOLE_OUTPUT:
-            print u'当前页面请求失败数', fail_time
-        if fail_time == config.MAX_FAIL:
-            if config.CONSOLE_OUTPUT:
-                print '失败次数过多, 跳过此请求'
-            return False
-        scrap(url, fail_time)
 
 
 def from_file():
