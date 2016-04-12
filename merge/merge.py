9456f54b6548c497a8d355b6fc151102e51018a2
cqc <1016903103@qq.com>
2016-04-12 14:59:38 +0800

    update merge info

--- merge/merge.py
+++ merge/merge.py
@@ -15,15 +15,21 @@ def merge_file():
     #print phones
     wb = copy(rb)
     write_sheet = wb.get_sheet(0)
+    success_count = 0
     count = 0
     for name in names:
         count = count + 1
+        print u'当前匹配第',count ,u'行表格', name
+        success = 0
         for phone in phones:
             info = phone.split(' ')
             if len(info) == 2:
                 first = info[0]
                 second = info[1]
                 if name == first:
+                    success = 1
+                    success_count = success_count + 1
+                    print u'当前成功匹配了', success_count, '位用户'
                     print u'匹配到用户', name
                     print u'电话号码', phone
                     if len(second) == 11:
@@ -31,6 +37,8 @@ def merge_file():
                         wb.save(excel)
                     else:
                         print u'电话号码信息不完整, 没有导入'
+        if not success:
+            print u'未找到匹配用户'
 
 
 
